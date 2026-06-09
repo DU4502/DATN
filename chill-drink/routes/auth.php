c98394e0fcdb277d018df948cac5f3e39c5b6dab
Lê Hà Nam <78285092+namtrung12@users.noreply.github.com>
2026-06-09 11:36:23 +0700

    Thêm đăng nhập xã hội bằng Google và Facebook

--- chill-drink/routes/auth.php
+++ chill-drink/routes/auth.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
 use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\FacebookController;
+use App\Http\Controllers\Auth\GoogleController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
@@ -21,6 +23,18 @@ Route::middleware('guest')->group(function () {
         ->name('login');
 
     Route::post('login', [AuthenticatedSessionController::class, 'store']);
+
+    Route::get('auth/google/redirect', [GoogleController::class, 'redirect'])
+        ->name('auth.google.redirect');
+
+    Route::get('auth/google/callback', [GoogleController::class, 'callback'])
+        ->name('auth.google.callback');
+
+    Route::get('auth/facebook/redirect', [FacebookController::class, 'redirect'])
+        ->name('auth.facebook.redirect');
+
+    Route::get('auth/facebook/callback', [FacebookController::class, 'callback'])
+        ->name('auth.facebook.callback');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
